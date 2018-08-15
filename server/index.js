79d115ca260ac9667dbbbb5358f0d0b653bca75d
jr <jr.evilboss@gmail.com>
2018-08-15 15:21:55 +0800

    add python module

--- server/index.js
+++ server/index.js
@@ -2,14 +2,16 @@ import express from 'express';
 import http from 'http';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import PythonShell from 'python-shell';
+
 const app = express();
 const server = http.createServer(app);
 dotenv.config();
 console.log("Server started");
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.all("/*", function(req, res, next) {
+app.use(bodyParser.urlencoded({extended: false}));
+app.all("/*", function (req, res, next) {
     // CORS headers
     res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
@@ -23,5 +25,22 @@ app.all("/*", function(req, res, next) {
     } else {
         next();
     }
+});
+app.get("/", function (req, res) {
+    res.send(" Real time POS web app running.");
+});
+app.get("/python", function (req, res) {
+    PythonShell.run('./python/sample.py', "", function (err, results) {
+        if (err)
+            throw err;
+        // Results is an array consisting of messages collected during execution
+        console.log(results.toString());
+        res.setHeader('Content-Type', 'application/json');
+        res.send((results).toString());
+
+
+    });
+
+
 });
 server.listen(process.env.port, () => console.log(`Listening on port ${process.env.port}`));
