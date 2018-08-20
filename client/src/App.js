39bac3b76897d5282a2f399ca9f7e076945c46be
jr <jr.evilboss@gmail.com>
2018-08-20 16:45:30 +0800

    remove multi-theme provider from material ui lib

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore, applyMiddleware } from 'redux'
+import {createStore, applyMiddleware} from 'redux'
 import {Provider} from 'react-redux'
 import thunk from 'redux-thunk'
 
 import reducer from './js/reducers/reducer'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import Main from "./js/components/Main";
-import DashboardPage from "views/Dashboard";
+
 const store = createStore(reducer, applyMiddleware(thunk));
 
 const App = () => (
     <Provider store={store}>
-        <MuiThemeProvider>
-            <Main/>
-        </MuiThemeProvider>
+        <Main/>
     </Provider>
 );
-
 export default App;
